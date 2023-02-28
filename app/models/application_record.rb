fe83bcda1035afd76a0daea44729f1272fbf0af5
shingz96 <kahshingchee@gmail.com>
2023-02-28 22:54:35 +0800

    temporary allow full ransack access

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,26 @@
 
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  class << self
+    def ransackable_attributes(auth_object = nil)
+      if allow_full_ransack_access?(auth_object)
+        authorizable_ransackable_attributes
+      else
+        super
+      end
+    end
+
+    def ransackable_associations(auth_object = nil)
+      if allow_full_ransack_access?(auth_object)
+        authorizable_ransackable_associations
+      else
+        super
+      end
+    end
+
+    def allow_full_ransack_access?(auth_object)
+      true # temporary allow now
+    end
+  end
 end
